a79231d34ea337365f829f2b05c816b089cc8d71
Ali Hesari <alihesari.com@gmail.com>
2019-06-11 13:26:13 +0430

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -4,12 +4,12 @@
 */
 function getHolidays() {
 	function parseNumbers(str) {
-        return Number( str.replace(/[٠١٢٣٤٥٦٧٨٩]/g, function(d) {
-            return d.charCodeAt(0) - 1632; // Convert Arabic numbers
-        }).replace(/[۰۱۲۳۴۵۶۷۸۹]/g, function(d) {
-            return d.charCodeAt(0) - 1776; // Convert Persian numbers
-        }) );
-    }
+		return Number( str.replace(/[٠١٢٣٤٥٦٧٨٩]/g, function(d) {
+		    return d.charCodeAt(0) - 1632; // Convert Arabic numbers
+		}).replace(/[۰۱۲۳۴۵۶۷۸۹]/g, function(d) {
+		    return d.charCodeAt(0) - 1776; // Convert Persian numbers
+		}) );
+	}
 
 	function jalaliMonthName(monthNumber) {
 		var months = ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"];
